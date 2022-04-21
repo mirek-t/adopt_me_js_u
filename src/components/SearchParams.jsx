6c1a8c1d91ae2f57793ce5e479f6fd85d3bbfbe5
Mirek Truminski <miroslaw.truminski@gmail.com>
2022-04-21 20:19:27 +0200

    get data from backend server

--- src/components/SearchParams.jsx
+++ src/components/SearchParams.jsx
@@ -1,14 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
 
 const animals = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 function SearchParams() {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const breeds = [];
+
+  useEffect(() => {
+    requestPets().catch(() => {});
+  }, []);
+
+  async function requestPets() {
+    const response = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const data = await response.json();
+    setPets(data.pets);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    requestPets();
+  }
 
   return (
     <div className="search-params">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="location">Location</label>
         <input
           type="text"
@@ -39,8 +61,34 @@ function SearchParams() {
           })}
         </select>
 
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          value={breed}
+          disabled={!breeds.length}
+          onChange={(event) => {
+            setBreed(event.target.value);
+          }}
+          onBlur={(event) => {
+            setBreed(event.target.value);
+          }}
+        >
+          <option />
+          {breeds.map((breed) => {
+            return (
+              <option value={breed} key={breed}>
+                {breed}
+              </option>
+            );
+          })}
+        </select>
+
         <button type="submit">Submit</button>
       </form>
+
+      {pets.map(({ name, animal, breed, id }) => (
+        <Pet key={name} name={name} animal={animal} breed={breed} />
+      ))}
     </div>
   );
 }
